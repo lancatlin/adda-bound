62839e34afdde8f1485137f0967e70598dac72ca
wancat <wancat@wancat.cc>
2021-05-07 17:41:45 +0800

    Add core app
    + Room model
    + create Room test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    '''Room is the basic unit in a chat, could be one or group'''
+
+    class Service(models.TextChoices):
+        LINE = 'LN', _('LINE')
+        MESSENGER = 'MS', _('Messenger')
+        TELEGRAM = 'TG', _('Telegram')
+        DISCORD = 'DC', _('Discord')
+
+    room_id = models.CharField(max_length=255)
+    service = models.CharField(choices=Service.choices, max_length=2)
+    name = models.CharField(max_length=255)
+    rooms = models.ManyToManyField('self', symmetrical=True)
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 COMMAND=$*
-if [ $1 == 'test' ]; then
+if [ $1 = 'test' ]; then
     COMMAND="python manage.py test && flake8"
 fi
 echo $COMMAND
-docker-compose run app sh -c "$*"
+docker-compose run app sh -c "$COMMAND"
