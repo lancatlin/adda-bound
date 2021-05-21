65741ddab8f717db2715bbf21e43cb561bc17df8
wancat <wancat@pm.me>
2021-05-21 21:53:06 +0800

    Add test
    - make sure rooms don't add each other automatically

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,7 @@ from core.models import Room
 def sample_room(**kwargs):
     payload = {
         'room_id': '1234567',
+        'room_type': 'U',
         'service': 'LN',
         'name': 'test',
     }
@@ -20,3 +21,9 @@ class RoomTests(TestCase):
         room1.rooms.add(room2)
         self.assertEqual(room1.rooms.count(), 1)
         self.assertEqual(room2.rooms.get(name=room1.name), room1)
+
+    def test_create_rooms_and_not_add_eachother(self):
+        room1 = sample_room(name='room1')
+        room2 = sample_room(name='room2')
+        self.assertEqual(room1.rooms.count(), 0)
+        self.assertEqual(room2.rooms.count(), 0)
