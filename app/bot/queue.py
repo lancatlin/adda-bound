15f9d5369af7108d1da8ce1e200b27216f4c3c6f
wancat <wancat@pm.me>
2021-06-06 10:09:09 +0800

    Queue could handle and request data

--- app/bot/queue.py
+++ app/bot/queue.py
@@ -1,11 +1,41 @@
-from queue import Queue
+from queue import Queue, Empty, Full
 from threading import Lock
+
+from .utils import with_room
 
 
 class MessageQueue:
     def __init__(self):
         self._queue = {}
         self._lock = Lock()
+        self._requests = {}
+        self._responses = {}
+
+    def create_if_not_exists(self, room):
+        if room.id not in self._requests:
+            self._requests[room.id] = Queue(maxsize=1)
+
+        if room.id not in self._responses:
+            self._responses[room.id] = Queue(maxsize=1)
+
+    def handle(self, event):
+        @with_room
+        def func(event, room):
+            self.create_if_not_exists(room)
+            if self._requests[room.id].get_nowait():
+                self._responses[room.id].put(event.message.text)
+
+        try:
+            func(event)
+        except Empty:
+            '''No request, ignore the message'''
+            pass
+
+    def request(self, room):
+        self.create_if_not_exists(room)
+
+        self._requests[room.id].put(True)
+        return self._responses[room.id].get()
 
     def with_lock(callback):
         def func(self, *arg, **kwargs):
--- app/bot/tests/test_queue.py
+++ app/bot/tests/test_queue.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 
+from threading import Thread
+import time
+
 from bot.queue import MessageQueue
 from core.tests.test_models import sample_room
+from bot.tests.test_line import sample_event
 
 
 class QueueTest(TestCase):
@@ -11,9 +15,22 @@ class QueueTest(TestCase):
 
     def test_put_message(self):
         '''Test put the message into queue'''
+        msg = 'test message'
+
+        def req():
+            res = self.queue.request(self.room)
+            self.assertEqual(res, msg)
+
+        thread = Thread(target=req)
+        thread.start()
+        self.queue.handle(sample_event(self.room, msg))
+        thread.join()
 
     def test_not_to_put_message(self):
         '''Test ignore the message when no request'''
+        msg = 'test message'
+        self.queue.handle(sample_event(self.room, msg))
+        self.assertTrue(self.queue._responses[self.room.id].empty())
 
     def test_request_message(self):
         '''Test request some message and get one'''
