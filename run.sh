6522b2a9601da5350f683db1b83554e6235d411c
wancat <wancat@pm.me>
2021-07-02 18:07:23 +0800

    Update run.sh (to make travis pass)

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 COMMAND=$*
 if [ $1 = 'test' ]; then
-    COMMAND="python manage.py test && flake8"
+    COMMAND="python manage.py wait_for_db && python manage.py test && flake8"
 fi
 echo $COMMAND
 docker-compose run app sh -c "$COMMAND"
